311fc85dada58453814cbfe577603caad497c3c9
Hadi <hadibakhshi277@gmail.com>
2022-02-20 23:59:02 +0330

    validation added

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,5 +1,19 @@
 import { useFormik } from "formik";
 
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "Name is required";
+  }
+  if (!values.email) {
+    errors.email = "Email is required";
+  }
+  if (!values.password) {
+    errors.password = "Password is required";
+  }
+  return errors;
+};
+
 const SignUpForm = () => {
   const formik = useFormik({
     initialValues: {
@@ -10,6 +24,7 @@ const SignUpForm = () => {
     onSubmit: (values) => {
       console.log(values);
     },
+    validate,
   });
 
   return (
@@ -21,8 +36,12 @@ const SignUpForm = () => {
             type="text"
             value={formik.values.name}
             name="name"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
           />
+          {formik.errors.name && formik.touched.name && (
+            <div className="error">{formik.errors.name}</div>
+          )}
         </div>
         <div className="formControl">
           <label>Email</label>
@@ -30,8 +49,12 @@ const SignUpForm = () => {
             type="email"
             value={formik.values.email}
             name="email"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
           />
+          {formik.errors.email && formik.touched.email && (
+            <div className="error">{formik.errors.email}</div>
+          )}
         </div>
         <div className="formControl">
           <label>Password</label>
@@ -39,8 +62,12 @@ const SignUpForm = () => {
             type="password"
             value={formik.values.password}
             name="password"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
           />
+          {formik.errors.password && formik.touched.password && (
+            <div className="error">{formik.errors.password}</div>
+          )}
         </div>
         <button type="submit">Sign up</button>
       </form>
